d41076f06f8e41f0819e63dc25ea142872fbb36a
marcobonn <Bonn.Marco@googlemail.com>
2017-10-19 16:43:20 +0200

    jo

--- Fussball/src/Stadion.java
+++ Fussball/src/Stadion.java
@@ -3,6 +3,7 @@ public class Stadion {
 public static void main(String[] args) {
 	
 	System.out.println("asd");
+	System.out.println("asd");
 	
 	Person[] Zuschauer = new Person[45001]; // <<<< Array erstellen
 	Fussballspieler[] Spieler_rot = new Fussballspieler [6]; // 0-4 = rot 5-10 blau
